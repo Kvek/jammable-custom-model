f1ef3a1d1b580bb4a7fdffdc2c761dc27ad9787e
vkavek <vkavek.dev@pm.me>
2024-04-15 16:40:10 +0100

    style: update upload button style

--- src/wrappers/sidebar/SidebarNav.tsx
+++ src/wrappers/sidebar/SidebarNav.tsx
@@ -1,14 +1,12 @@
-import { Library, Mic, Pencil, ShuffleIcon } from "lucide-react";
+import { Library, Mic, Pencil, PlusIcon, ShuffleIcon } from "lucide-react";
 import React from "react";
 
 import { Button } from "@components/ui/button";
 
-import { PlusIcon } from "@radix-ui/react-icons";
-
 import { SidebarNavItems } from "./SidebarNavItems";
 
 const SidebarNav = (): JSX.Element => (
-  <div className="rounded-primary flex h-full w-full flex-col space-y-10 bg-primary p-4">
+  <div className="flex h-full w-full flex-col space-y-10 rounded-primary bg-primary p-4">
     <div className="flex items-center justify-between">
       <h1 className="flex items-end space-x-1">
         <Library height={35} width={35} />
@@ -16,10 +14,10 @@ const SidebarNav = (): JSX.Element => (
       </h1>
 
       <Button
-        className="from-button-pink to-button-purple flex items-center space-x-1 bg-gradient-to-r p-1.5 opacity-95 hover:opacity-100"
+        className="flex items-center space-x-1 bg-gradient-to-r from-button-pink to-button-purple p-1.5 opacity-95 hover:opacity-100"
         size={"shell"}
         variant={"shell"}>
-        <PlusIcon className="font-semibold" height={15} width={15} />
+        <PlusIcon height={15} strokeWidth={3} width={15} />
         <span>Upload Voice</span>
       </Button>
     </div>
